4b378c5c63b7fb900594d8063e4a7d9a2c64dbbf
Robin Dong <robin.k.dong@gmail.com>
2025-06-05 21:32:41 +0000

    Reach 77.95% for pretraining when drop-path is 0.0

--- train.sh
+++ train.sh
@@ -1,22 +1,22 @@
-DATA_PATH=/data/robin/imagenet
+DATA_PATH=/root/imagenet
 LOCAL_RANK=0
 
 ALL_BATCH_SIZE=4096
-NUM_GPU=1
-GRAD_ACCUM_STEPS=8 # Adjust according to your GPU numbers and memory size.
+NUM_GPU=2
+GRAD_ACCUM_STEPS=4 # Adjust according to your GPU numbers and memory size.
 BATCH_SIZE=$(expr $ALL_BATCH_SIZE / $NUM_GPU / $GRAD_ACCUM_STEPS )
 
-torchrun --nproc_per_node=1 train.py $DATA_PATH \
+torchrun --nproc_per_node=2 train.py $DATA_PATH \
 --model poolformerv2_s12 \
 --opt adamw \
---lr 4e-4 \
+--lr 1e-2 \
+--min-lr 1e-6 \
 --warmup-epochs 5 \
 --batch-size $BATCH_SIZE \
 --grad-accum-steps $GRAD_ACCUM_STEPS \
---drop-path 0.2 \
+--drop-path 0.0 \
 --head-dropout 0.0 \
---log-interval 2000 \
---epochs 100 \
---start-epoch 0 \
---no-resume-opt \
---resume poolformerv2_s12.pth
+--workers 32 \
+--pin-mem \
+--log-interval 1000 \
+--epochs 300
