d205ed3b6c2799f4ab3fef8f623c50d810404cc1
Robin Dong <robin.k.dong@gmail.com>
2025-06-16 11:20:58 +1000

    Add ECAModule into DownSampling

--- metaformer_baselines.py
+++ metaformer_baselines.py
@@ -190,6 +190,32 @@ default_cfgs = {
 }
 
 
+class eca_layer(nn.Module):
+    """Constructs a ECA module.
+
+    Args:
+        channel: Number of channels of the input feature map
+        k_size: Adaptive selection of kernel size
+    """
+    def __init__(self, channel, k_size=3):
+        super(eca_layer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # feature descriptor on the global spatial information
+        y = self.avg_pool(x)
+
+        # Two different branches of ECA module
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+
+        # Multi-scale information fusion
+        y = self.sigmoid(y)
+
+        return x * y.expand_as(x)
+
+
 class Downsampling(nn.Module):
     """
     Downsampling implemented by a layer of convolution.
@@ -203,6 +229,7 @@ class Downsampling(nn.Module):
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, 
                               stride=stride, padding=padding)
         self.post_norm = post_norm(out_channels) if post_norm else nn.Identity()
+        self.eca = eca_layer(out_channels)
 
     def forward(self, x):
         x = self.pre_norm(x)
@@ -210,6 +237,7 @@ class Downsampling(nn.Module):
             # if take [B, H, W, C] as input, permute it to [B, C, H, W]
             x = x.permute(0, 3, 1, 2)
         x = self.conv(x)
+        x = self.eca(x)
         x = x.permute(0, 2, 3, 1) # [B, C, H, W] -> [B, H, W, C]
         x = self.post_norm(x)
         return x
