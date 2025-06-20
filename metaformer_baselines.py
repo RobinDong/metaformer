f3efa185bed8c6529ee4f3e5c9bae9bb6c2b1dec
Robin Dong <robin.k.dong@gmail.com>
2025-06-20 15:16:48 +1000

    Add Coord-Attention to replace avg-pool

--- metaformer_baselines.py
+++ metaformer_baselines.py
@@ -18,6 +18,7 @@ ConvFormer and CAFormer.
 Some implementations are modified from timm (https://github.com/rwightman/pytorch-image-models).
 """
 from functools import partial
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -410,6 +411,63 @@ class SepConv(nn.Module):
         return x
 
 
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class CoordAtt(nn.Module):
+    def __init__(self, inp, oup, reduction=32):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        mip = max(8, inp // reduction)
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+
+        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+
+
+    def forward(self, x):
+        identity = x
+
+        n,c,h,w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y)
+
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = identity * a_w * a_h
+
+        return out
+
+
 class Pooling(nn.Module):
     """
     Implementation of pooling for PoolFormer: https://arxiv.org/abs/2111.11418
@@ -420,20 +478,17 @@ class Pooling(nn.Module):
         self.pool = nn.AvgPool2d(
             pool_size, stride=1, padding=pool_size//2, count_include_pad=False)
 
-        k_size=3
+        '''k_size=3
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
-        self.sigmoid = nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid()'''
+        self.ca = CoordAtt(kwargs['dim'], kwargs['dim'])
 
     def forward(self, x):
         y1 = x.permute(0, 3, 1, 2)
 
-        y2 = self.avg_pool(y1)
-        y2 = self.conv(y2.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
-        y2 = self.sigmoid(y2)
-        y2 = y1 * y2.expand_as(y1)
-
-        y1 = self.pool(y1) + y2
+        y1 = self.ca(y1)
+        # y1 = self.pool(y1) + y2
         #print("y1, y2:", y1.size(), y2.size())
         # y1, y2: torch.Size([8192, 64, 14, 14]) torch.Size([8192, 64, 14, 14])
         y1 = y1.permute(0, 2, 3, 1)
